be786143d8ced7e46a78303a77d11ca4461a2c3e
Josy Pinheiro <josyanna.pinheiro@gmail.com>
2024-03-26 11:12:48 -0300

    produtos

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,22 +1,50 @@
 /// <reference types="cypress"/>
 
+
+import produtosPage from "../../support/page-objects/produtos.page";
+
+
 describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     
     });
         it('Deve selecionar um produto da lista', () => {    
-        
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
+        produtosPage.buscarProdutoLista('Aether Gym Pant')
 
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
             
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarPruduto('Augusta Pullover Jacket')
+
+        cy.get('.product_title').should('contain', 'Augusta Pullover Jacket')
+    });
   
-    
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Augusta Pullover Jacket')
+
+        cy.get('.product_title').should('contain', 'Augusta Pullover Jacket')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarPruduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('XS', 'Red', qtd)
+
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.' )
+
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarPruduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto) 
+        })
+    });
 });
